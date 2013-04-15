bd4029396c0e3ee65f8c48e104cf787063cc6a25
Alberto Valverde <alberto@meteogrid.com>
2013-04-15 10:59:05 -0700

    vectorize jits with nopython

--- ufuncexpr/vectorize.py
+++ ufuncexpr/vectorize.py
@@ -2,9 +2,10 @@ from numba import jit
 from numba.codegen.llvmcontext import LLVMContextManager
 from .builder import make_ufunc
 
-def vectorize(signatures):
+def vectorize(signatures, **kw):
+    kw['nopython'] = True # Using python inside UFuncs seems to segfault, perhaps GIL related? investigate...
     def wrap(f):
-        llvm_functions = [jit(s)(f).lfunc for s in signatures]
+        llvm_functions = [jit(s, **kw)(f).lfunc for s in signatures]
         engine = LLVMContextManager().execution_engine
         return make_ufunc(llvm_functions, engine, f.__name__, f.__doc__ or '')
     return wrap
